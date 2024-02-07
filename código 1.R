c4edbad98f193157f93e4f43b2e7685a02cfdf56
ceci-cabello <rosa.azulina@gmail.com>
2024-02-07 12:05:55 -0600

    comit 2 hasta variable clutch completion

--- código 1.R
+++ código 1.R
@@ -23,4 +23,28 @@ datos$`Sample Number`
 #variable3: species
 levels(factor(datos$Species))
 
-#variable4: 
+datos$Species <- ifelse(datos$Species == "Gento penguin (Pygoscelis papua)", "Gentoo penguin (Pygoscelis papua)", datos$Species)
+datos$Species <- ifelse(datos$Species == "Adeli Penguin (Pygoscelis adeliae)", "Adelie Penguin (Pygoscelis adeliae)", datos$Species)
+datos$Species <- ifelse(datos$Species == "ADELIE PENGUIN (PYGOSCELIS ADELIAE)", "Adelie Penguin (Pygoscelis adeliae)", datos$Species)
+
+
+#variable4: región
+levels(factor(datos$Region))
+#una única región "Anvers"
+
+#variable5: island
+levels(factor(datos$Island))
+#3 islas: Biscoe, Dream y Torgersen
+
+#variable6: stage
+levels(factor(datos$Stage))
+#sin errores
+
+
+#variable7: IndividualID
+datos$`Individual ID`
+
+
+#variable8: clutch completion
+levels(factor(datos$`Clutch Completion`))
+#sin errores
