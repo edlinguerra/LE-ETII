e3ad6d3574afd6bbfd077694bcce4d039bda7eef
ceci-cabello <rosa.azulina@gmail.com>
2024-02-09 18:12:27 -0600

    final de la tarea

--- código 1.R
+++ código 1.R
@@ -48,3 +48,68 @@ datos$`Individual ID`
 #variable8: clutch completion
 levels(factor(datos$`Clutch Completion`))
 #sin errores
+
+#variable9: dateegg
+datos$`Date Egg`
+
+#Variable9.1: year
+datos$year <- substr(datos$`Date Egg`, 1, 4)
+datos
+#la nueva columna con solo los años se agregó al final del data frame
+
+#Variable10: culmenlength
+datos$`Culmen Length (mm)`
+#sin errores
+
+#Variable11: culmendepth
+datos$`Culmen Depth (mm)`
+#sin errores
+
+#Variable12: flipperlength
+datos$`Flipper Length (mm)`
+plot(datos$`Flipper Length (mm)`)
+#sin errores
+
+#Variable13: boddy mass
+datos$`Body Mass (g)`
+plot(datos$`Body Mass (g)`)
+#sin errores
+
+#Variable14: sex
+levels(factor(datos$Sex))
+#hay error, algunos datos vienen en mayúsculas y otras en minúsculas
+
+datos$Sex <- ifelse(datos$Sex == "Female", "FEMALE", datos$Sex)
+datos$Sex <- ifelse(datos$Sex == "Male", "MALE", datos$Sex)
+#problema arreglado
+
+#variable15: delta15
+datos$`Delta 15 N (o/oo)`
+#sin errores
+
+#Variable16: delta13
+datos$`Delta 13 C (o/oo)`
+#sin errores
+
+#variable comentarios
+levels(factor(datos$Comments))
+#sin errores
+
+
+#GRÁFICOS DE ALGUNOS DE LOS DATOS
+
+library(ggplot2)
+
+colnames(datos)
+
+#distribución de masa corporal por sexo
+datos %>%
+  ggplot( aes(x=Sex, y=`Body Mass (g)`, fill= Sex)) +
+  geom_boxplot()
+
+#distribución de longitud de ala por sexo
+datos %>%
+  ggplot( aes(x=Sex, y= `Flipper Length (mm)`, fill= Sex)) +
+  geom_boxplot()
+
+
